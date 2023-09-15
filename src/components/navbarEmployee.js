b4d0c496545627fd27f8b19124902e595804dc86
CuongHD98 <you@example.com>
2023-09-15 10:17:24 +0700

    up component

--- src/components/navbarEmployee.js
+++ src/components/navbarEmployee.js
@@ -80,11 +80,6 @@ const NavbarEmployee = () => {
                                                         <line x1={3} y1={6} x2={21} y2={6}/>
                                                         <path d="M16 10a4 4 0 0 1-8 0"/>
                                                     </svg>
-                      {/*                              <span*/}
-                      {/*                                  className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">*/}
-                      {/*  1*/}
-                      {/*  <span className="visually-hidden">unread messages</span>*/}
-                      {/*</span>*/}
                                                 </Link>
                                             </div>
                                         </div>
@@ -149,30 +144,6 @@ const NavbarEmployee = () => {
                             <div className="col-md-2 col-xxl-2 text-end d-none d-lg-block">
                                 <div className="list-inline">
                                     <div className="list-inline-item me-5">
-                                        <Link to={"/product/wishlist"}>
-                                            <a
-                                                href=""
-                                                className="text-muted position-relative"
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width={20}
-                                                    height={20}
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeWidth={2}
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    className="feather feather-heart"
-                                                >
-                                                    <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                                                </svg>
-                                            </a>
-                                        </Link>
-                                    </div>
-
-                                    <div className="list-inline-item me-5">
                                         <Link to={"/signin"}>
                                             <a
                                                 href="#!"
@@ -196,51 +167,6 @@ const NavbarEmployee = () => {
                                             </a>
                                         </Link>
                                     </div>
-                                    <div className="list-inline-item">
-                                        <Link
-                                            className="text-muted position-relative "
-                                            to={"/cart"}
-                                            role="button"
-                                            aria-controls="offcanvasRight"
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width={20}
-                                                height={20}
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                                stroke="currentColor"
-                                                strokeWidth={2}
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                className="feather feather-shopping-bag"
-                                            >
-                                                <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/>
-                                                <line x1={3} y1={6} x2={21} y2={6}/>
-                                                <path d="M16 10a4 4 0 0 1-8 0"/>
-                                            </svg>
-                  {/*                          <span*/}
-                  {/*                              className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">*/}
-                  {/*  1<span className="visually-hidden">unread messages</span>*/}
-                  {/*</span>*/}
-                                        </Link>
-                                    </div>
-                                    &nbsp;&nbsp;
-                                    <div className="list-inline-item">
-                                        <a
-                                            className="text-muted position-relative"
-                                            data-bs-toggle="offcanvas"
-                                            data-bs-target="#offcanvasRight"
-                                            href="#offcanvasExample"
-                                            role="button"
-                                            aria-controls="offcanvasRight"
-                                        >
-                                            <svg fill="#777777" width="20px" height="20px" viewBox="0 0 16 16" id="bell-16px" xmlns="http://www.w3.org/2000/svg">
-                                                <path id="Path_164" data-name="Path 164" d="M42.2,10.7l-1.53-1.02A1.5,1.5,0,0,1,40,8.43V6a5,5,0,0,0-3.109-4.623,1.987,1.987,0,0,0-3.782,0A5,5,0,0,0,30,6V8.43a1.5,1.5,0,0,1-.668,1.248L27.8,10.7a1.8,1.8,0,0,0,1,3.3H33a2,2,0,0,0,4,0h4.2a1.8,1.8,0,0,0,1-3.3Zm-1,2.3H28.8a.8.8,0,0,1-.444-1.47l1.531-1.02A2.5,2.5,0,0,0,31,8.43V6a4,4,0,0,1,8,0V8.43a2.5,2.5,0,0,0,1.113,2.08l1.532,1.021A.8.8,0,0,1,41.2,13ZM35,15a1,1,0,0,1-1-1h2A1,1,0,0,1,35,15Zm.5-11.5A.5.5,0,0,1,35,4a2,2,0,0,0-2,2,.5.5,0,0,1-1,0,3,3,0,0,1,3-3A.5.5,0,0,1,35.5,3.5Z" transform="translate(-27)"/>
-                                            </svg>
-                                        </a>
-                                    </div>
-
                                 </div>
                             </div>
                         </div>
@@ -518,14 +444,7 @@ const NavbarEmployee = () => {
                                             </Link>
                                         </li>
                                         <li className="nav-item w-100 w-lg-auto">
-                                            <Link to={"/order"}>
-                                                <a className="nav-link" href="">
-                                                    Order
-                                                </a>
-                                            </Link>
-                                        </li>
-                                        <li className="nav-item w-100 w-lg-auto">
-                                            <Link to={"/setting"}>
+                                            <Link to={"/employee/setting"}>
                                                 <a className="nav-link" href="">
                                                     Setting
                                                 </a>
---
+++ src/outlet/employee/employeeSetting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const EmployeeSetting = () => {
+    return (
+        <></>
+    );
+};
+
+export default EmployeeSetting;
